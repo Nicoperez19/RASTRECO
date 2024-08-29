b22fbfd0f684e0822ea844b342a8a9faf156ca29
Nicoperez19 <nperez@ing.ucsc.cl>
2024-08-29 02:32:20 -0400

    Update 2024_08_28_203352_create_sensor_table.php

--- database/migrations/2024_08_28_203352_create_sensor_table.php
+++ database/migrations/2024_08_28_203352_create_sensor_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('sensor', function (Blueprint $table) {
             $table->bigIncrements('id'); // Clave primaria auto-incrementable
+            $table->integer('id_arduino');
             $table->float('temperature'); // Temperatura
             $table->float('humidity'); // Humedad
             $table->string('status_read_sensor_dht11'); // Estado del sensor DHT11
